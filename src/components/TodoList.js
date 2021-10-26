520f2031d685d3018ab992068f6653e9e7f8875e
Muhi Masri <Muhi.Masri@mdacorporation.com>
2021-10-26 13:41:29 -0400

    Todo dictionary

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./todo.css";
 
 export const TodoList = () => {
   const [todoList, setTodoList] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function getTodoList() {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/todos"
       );
-      setTodoList(await response.json());
+      const data = await response.json();
+      setTodoList(data);
+      dispatch({
+        type: 'UPDATE_TODO_LIST',
+        value: data
+      })
     }
     getTodoList();
   }, []);
--- src/store/todoReducer.js
+++ src/store/todoReducer.js
@@ -1,17 +1,10 @@
 const initialState = {
-  todoList: [
-    { id: 1, title: "Do groceries", completed: false },
-    { id: 2, title: "Do shopping", completed: false },
-  ],
 };
 
 export const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_TODO_LIST": {
-      return {
-        ...state,
-        todoList: [...action.value],
-      };
+      return action.value.reduce((accumulator, current) => ({...accumulator, [current.id]: current}), {});
     }
     default:
       return state;
